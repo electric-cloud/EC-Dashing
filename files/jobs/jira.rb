7f3da40b53197742810321654d681b7fdcc8c793
Siddhartha <sgupta9020@gmail.com>
2015-01-28 14:52:34 -0800

    Reducing JIRA server polling time
    The polling time was too frequent,

--- files/jobs/jira.rb
+++ files/jobs/jira.rb
@@ -12,7 +12,7 @@ points = []
 board_id = '504' # The ID number of the Agile Board you wish to monitor
 wip_status = 'In Progress'
 
-SCHEDULER.every '5s', :first_in => 0 do |job|
+SCHEDULER.every '10m', :first_in => 0 do |job|
   uri = URI.parse($jira_url)
   http = Net::HTTP.new(uri.host, uri.port)
   req = Net::HTTP::Get.new("/rest/api/2/search?jql=sprint%20%3D%20#{board_id}%20AND%20status%20%3D%20%27In%20Progress%27")
